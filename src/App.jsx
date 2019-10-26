eb8e94348d43fa001863e6eebdd4dba1657ec3aa
Sunita <54287247+sunitase@users.noreply.github.com>
2019-10-26 14:12:55 -0700

    fetch api and layout there

--- src/App.jsx
+++ src/App.jsx
@@ -1,50 +1,62 @@
 import React, { Component } from 'react';
-//import photo from './photo'
 
-class app extends Component {
+class App extends Component {
     constructor(props){
         super(props);
-        //state = {
-          //  photo: ""
-          //}; bind required
-
 }
 
 componentDidMount(){
 
         
-            fetch(`https://api.nasa.gov/planetary/apod?api_key=1gPUiHV0m85zO9DCG6ssTie5bYmKi7cL9XvJgiEm`)
-              .then(response => response.json())
-              .then(data => console.log(data));
-              //.then(json => this.setState({ photo: json }));
+        fetch(`https://api.nasa.gov/planetary/apod?api_key=1gPUiHV0m85zO9DCG6ssTie5bYmKi7cL9XvJgiEm`)
+        .then(response => response.json())
+        .then(data => console.log(data))
+        //picture of the day
+
+        fetch("http://api.open-notify.org/astros.json")
+            .then(response => response.json())
+            .then(data => console.log(data['number']))
+              
           
   
         fetch("https://corporatebs-generator.sameerkumar.website/")
-        .then(response => response.json())
+            .then(response => response.json())
             .then(data => console.log(data))
 
-            fetch("https://geek-jokes.sameerkumar.website/api")
+        fetch("https://geek-jokes.sameerkumar.website/api")
             .then(response => response.json())
-                .then(data => console.log(data)) 
+            .then(data => console.log(data)) 
         
-            fetch("http://api.open-notify.org/astros.json")
-            .then(response => response.json())
-                .then(data => console.log(data))
-
-        
+            
 }
 
 
 render(){
+    var uri;
     return(
-            <div className="container">
+        <div className="container">
+
+         <div class="row">
+
+            <div class="col-5">
+                <div class="d-flex">
                 <div>
-        <h1>NASA's Astronomy Picture of the Day</h1>
-       <h4> 'Trying stuff out here'</h4>
-      </div>
+                  <h1>NASA's Astronomy Picture of the Day</h1>
+                  <h4> 'Trying stuff out here'</h4>
+                </div>
+                </div>
+             </div>
+  
+            <div class="col-5">
+                <div class="d-flex align-items-start">
+                 <div><h1>People currently in space</h1></div>
+                 </div>
             </div>
+             </div>
+        </div>
+    
     )
 }
 };
 
-export default app;
+export default App;

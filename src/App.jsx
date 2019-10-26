bba948096a55416f6b8dc0f6c20d0ada96c2f68d
Sunita <54287247+sunitase@users.noreply.github.com>
2019-10-26 07:23:38 -0700

    bunch of APIs in app for now

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,50 @@
 import React, { Component } from 'react';
+//import photo from './photo'
 
-class App extends Component {
+class app extends Component {
     constructor(props){
         super(props);
+        //state = {
+          //  photo: ""
+          //}; bind required
+
 }
+
+componentDidMount(){
+
+        
+            fetch(`https://api.nasa.gov/planetary/apod?api_key=1gPUiHV0m85zO9DCG6ssTie5bYmKi7cL9XvJgiEm`)
+              .then(response => response.json())
+              .then(data => console.log(data));
+              //.then(json => this.setState({ photo: json }));
+          
+  
+        fetch("https://corporatebs-generator.sameerkumar.website/")
+        .then(response => response.json())
+            .then(data => console.log(data))
+
+            fetch("https://geek-jokes.sameerkumar.website/api")
+            .then(response => response.json())
+                .then(data => console.log(data)) 
+        
+            fetch("http://api.open-notify.org/astros.json")
+            .then(response => response.json())
+                .then(data => console.log(data))
+
+        
+}
+
 
 render(){
     return(
             <div className="container">
-                <h1>Movie Night Combo</h1>
-
+                <div>
+        <h1>NASA's Astronomy Picture of the Day</h1>
+       <h4> 'Trying stuff out here'</h4>
+      </div>
             </div>
     )
 }
 };
 
-export default App;
+export default app;
